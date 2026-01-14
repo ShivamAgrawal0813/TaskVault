8f5edc5f90472ed7fbe849ee7c06d7a9765c8ebc
Shivam Agrawal <142653516+ShivamAgrawal0813@users.noreply.github.com>
2026-01-14 20:20:39 +0530

    Database Connection

--- backend/src/server/index.js
+++ backend/src/server/index.js
@@ -1,15 +1,25 @@
-import 'dotenv/config'
-// console.log(process.env);
-
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
 import { createServer } from 'node:http';
 
+dotenv.config();
+// console.log("MONGO_URI:", process.env.MONGO_URI);
+// // console.log(process.env);
+try{
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("Connected to MongoDB");
+}catch(err){
+  console.error("Error Connecting to MongoDB at server start: ",err.message);
+  process.exit(1);
+}
+
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT;
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World!');
 });
 
 server.listen(port, hostname, () => {
